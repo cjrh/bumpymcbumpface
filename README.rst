3e0543c280dfa56414a71d78eb029ffb55b8b9af
Caleb Hattingh <caleb.hattingh@gmail.com>
2019-04-30 21:07:02 +1000

    First version

---
+++ README.rst
@@ -0,0 +1,74 @@
+.. image:: https://img.shields.io/pypi/pyversions/${package}.svg
+    :target: https://pypi.python.org/pypi/${package}
+
+.. image:: https://img.shields.io/github/tag/cjrh/${package}.svg
+    :target: https://img.shields.io/github/tag/cjrh/${package}.svg
+
+.. image:: https://img.shields.io/badge/install-pip%20install%20$bumpymcbumpface-ff69b4.svg
+    :target: https://img.shields.io/badge/install-pip%20install%20$bumpymcbumpface-ff69b4.svg
+
+.. image:: https://img.shields.io/pypi/v/bumpymcbumpface.svg
+    :target: https://img.shields.io/pypi/v/bumpymcbumpface.svg
+
+.. image:: https://img.shields.io/badge/calver-YYYY.MM.MINOR-22bfda.svg
+    :target: http://calver.org/
+
+
+Bumpy McBumpface
+================
+
+When doing a new release, I got real tired of doing these things by hand:
+
+1. Update the version number of my package
+2. Commit that change to git
+3. Create a git tag with the new version
+4. Push the tag (and the new commit) to github
+5. Build new distributable packages: wheel (``.whl``) and source dist (``.tar.gz``)
+6. Upload the new packages to pypi
+
+*Bumpy McBumpface* will do these things, and will not ask questions.
+
+Requirements
+------------
+
+You need to have these:
+
+- A file called ``VERSION`` in your project root, with MAJOR.MINOR.PATCH numbers
+- git
+- twine (set up *keyring* so that twine won't ask you for login/password)
+- pip
+- wheel
+
+You probably SHOULD BE in master branch branch when you run, but you
+only need to be in a branch that has a valid remote.
+
+Instructions
+------------
+
+There are a several options, but only two important ones. By default,
+it will NOT do these things automatically:
+
+    - Push to github
+    - Deploy to PyPI
+
+This is just so that it defaults to "safe" by not affecting anything
+beyond your computer during testing. To make it do these things too,
+run it like this:
+
+    $ bumpymcbumpface --push-git --push-pypi
+
+*Bumpy McBumpface* assumes that your version number is in the following
+format:
+
+    MAJOR.MINOR.PATCH
+
+where each of the three fields is an integer. This also works for
+calendar-based versioning. By default, the PATCH value will be increased
+by 1 if you run without arguments. These two are the same:
+
+    $ bumpymcbumpface --push-git --push-pypi
+    $ bumpymcbumpface --push-git --push-pypi patch
+
+For a MINOR bump, change the work *patch* to *minor*. Likewise for *major*.
+
+Good luck!
---
+++ bumpymcbumpface.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python3
+# Copyright (C) 2019  Caleb Hattingh
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published
+# by the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+"""
+bumpymcbumpface - version-bump, tag, push, deploy
+"""
+
+import os
+import sys
+import shlex
+import argparse
+import subprocess as sp
+
+folder = os.getcwd()
+
+
+def main():
+    class MyHelpFormatter(
+            argparse.RawDescriptionHelpFormatter,
+            argparse.ArgumentDefaultsHelpFormatter):
+        pass
+    parser = argparse.ArgumentParser(
+        description=__doc__,
+        formatter_class=MyHelpFormatter,
+    )
+    parser.add_argument("--debug", action="store_true", help="Print debugging messages")
+    parser.add_argument("--show", action="store_true", help="Show the current version and exit")
+    parser.add_argument("--dry-run", action="store_true", help="Describe what would be done, and exit")
+    parser.add_argument("--push-git", action="store_true", help="Push the new commit and tag to github")
+    parser.add_argument("--push-pypi", action="store_true", help="Push the new built packages to PyPI")
+    parser.add_argument(
+        "field",
+        choices=["major", "minor", "patch"],
+        default="patch",
+        const="patch",
+        nargs="?",
+    )
+    args = parser.parse_args()
+
+    args.debug and print(args)
+    args.dry_run and print("Dry run active!")
+
+    version_filename = os.path.join(folder, "VERSION")
+    version = open(version_filename, encoding="utf-8").readline().strip()
+    if args.show:
+        print(version)
+        return
+
+    major, minor, patch = version.split(".")[:3]
+    if "major" in args.field:
+        major, minor, patch = int(major) + 1, 0, 0
+    if "minor" in args.field:
+        minor, patch = int(minor) + 1, 0
+    if "patch" in args.field or not args.field:
+        patch = int(patch) + 1
+    new_version = "{major}.{minor}.{patch}".format(
+        major=major, minor=minor, patch=patch)
+
+    git_status_output = sp.check_output("git status".split())
+    print(git_status_output)
+    if b"Changes not staged for commit:" in git_status_output:
+        print("Repo has uncommitted changes. Cannot continue")
+        sys.exit(1)
+
+    if b"Untracked files:" in git_status_output:
+        print("Repo has untracked files. Cannot continue")
+        sys.exit(1)
+
+    if args.dry_run:
+        print("The new version that would be written: {}".format(new_version))
+        return
+
+    with open(version_filename, "w", encoding="utf-8") as f:
+        f.write(new_version)
+
+    sp.check_call("git add {}".format(version_filename).split(), cwd=folder)
+    sp.check_call(shlex.split("git commit -m 'Bump version to {}'".format(new_version)), cwd=folder)
+    sp.check_call("git tag v{}".format(new_version).split(), cwd=folder)
+    if args.push_git:
+        sp.check_call("git push".split(), cwd=folder)
+        sp.check_call("git push --tags".split(), cwd=folder)
+
+    sp.run("{} setup.py bdist_wheel sdist".format(sys.executable).split(), cwd=folder)
+    if args.push_pypi:
+        sp.run("twine upload --skip-existing dist/*{}*".format(new_version), cwd=folder)
+
+
+if __name__ == "__main__":
+    main()
